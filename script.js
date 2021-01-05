d25b1ee0f8b3487d7c8018058c3fe60804e30636
JordanR94 <44776172+JordanR94@users.noreply.github.com>
2021-01-05 22:42:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector('#mobile-menu');
+var menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
